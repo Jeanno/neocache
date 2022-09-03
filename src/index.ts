7c5a831adda9d68af209a537ed0f742bf90af6dc
Jeanno <jeanno.cheung@gmail.com>
2022-09-02 20:51:44 -0700

    First commit

---
+++ src/index.ts
@@ -0,0 +1,60 @@
+const DEFAULT_EXPIRE_TIME_MS = 3600000;
+
+type CacheEntry = {
+  data: any;
+  expireTime: number;
+};
+
+export class Memcache {
+  private cache = new Map<string, CacheEntry>();
+  async get(id: string, fetchFunc: () => any) {
+    if (!id) {
+      return null;
+    }
+    const cache = this.cache[id];
+    if (cache && cache.expireAt > Date.now()) {
+      return cache.data;
+    }
+
+    if (!fetchFunc) {
+      return null;
+    }
+
+    const data = await fetchFunc();
+    this.set(id, data);
+    return data;
+  }
+
+  /**
+   * Returns a certain amount of random items from the cache.
+   * They must not be expired.
+   */
+  async getRandomItems(count: number) {
+    const keys = Object.keys(this.cache);
+    const randomKeys = keys.sort(() => 0.5 - Math.random());
+    const ret = [];
+    while (ret.length < count && randomKeys.length > 0) {
+      const key = randomKeys.pop();
+      const cache = this.cache[key];
+      if (cache.expireAt > Date.now()) {
+        ret.push(cache.data);
+      }
+    }
+    return ret;
+  }
+
+  set(id: string, data: any) {
+    this.cache[id] = {
+      data,
+      expireAt: Date.now() + DEFAULT_EXPIRE_TIME_MS,
+    };
+  }
+
+  invalidate(id: string) {
+    delete this.cache[id];
+  }
+}
+
+const defaultMemcache = new Memcache();
+
+export default defaultMemcache;
