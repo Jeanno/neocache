e7964fd1d7537cc5f2ea1adfafae83db2dde6cac
Jeanno <jeanno.cheung@gmail.com>
2023-03-11 17:39:30 -0800

    Purge expired item periodically

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,7 @@
 import Neocache from './index';
 
+jest.setTimeout(10000);
+
 test('smoke test', () => {
   expect(true).toBe(true);
 });
@@ -9,6 +11,7 @@ test('get', async () => {
   expect(await cache.get('foo')).toBeNull();
   const data = await cache.get('foo', () => 'bar');
   expect(data).toBe('bar');
+  cache.dispose();
 });
 
 test('singleton', () => {
@@ -18,12 +21,33 @@ test('singleton', () => {
   expect(cache1).toBeInstanceOf(Neocache);
   // Not null or undefined
   expect(cache1).toBeTruthy();
+  cache1.dispose();
 });
 
 test('expire time', async () => {
   const cache = new Neocache();
-  const data = await cache.get('foo', () => 'bar', { expireTimeMs: 100 });
+  const data = await cache.get('foo', () => 'bar', { expireTimeMs: 50 });
   expect(data).toBe('bar');
-  await new Promise((resolve) => setTimeout(resolve, 200));
+  await new Promise((resolve) => setTimeout(resolve, 100));
   expect(await cache.get('foo')).toBeNull();
+  cache.dispose();
+});
+
+test('purge expired', async () => {
+  const cache = new Neocache({
+    defaultExpireTimeMs: 50,
+    purgeIntervalMs: 100,
+  });
+  const data = await cache.get('foo', () => 'bar');
+  expect(data).toBe('bar');
+  expect(cache.length).toBe(1);
+  await new Promise((resolve) => setTimeout(resolve, 30));
+  expect(await cache.get('foo')).toBe('bar');
+  expect(cache.length).toBe(1);
+  await new Promise((resolve) => setTimeout(resolve, 30));
+  expect(await cache.get('foo')).toBeNull();
+  expect(cache.length).toBe(1);
+  await new Promise((resolve) => setTimeout(resolve, 200));
+  expect(cache.length).toBe(0);
+  cache.dispose();
 });
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,53 @@
-const DEFAULT_EXPIRE_TIME_MS = 3600000;
-
-type CacheEntry = {
+type CacheItem = {
   data: any;
   expireTime: number;
 };
 
-type CacheOptions = {
+type CacheItemOptions = {
   expireTimeMs?: number;
+};
+
+type CacheOptions = {
+  defaultExpireTimeMs?: number;
+  purgeIntervalMs?: number;
 };
 
 export class Neocache {
   static instance = new Neocache();
 
-  private cache = new Map<string, CacheEntry>();
+  private options: CacheOptions = {
+    defaultExpireTimeMs: 3600000,
+    purgeIntervalMs: 60000,
+  };
 
-  async get(id: string, fetchFunc?: () => any, options?: CacheOptions) {
+  private cache = new Map<string, CacheItem>();
+
+  private timeToKeyBucket = new Map<number, string[]>();
+  private purgeExpiredTimer: NodeJS.Timeout | null = null;
+
+  constructor(options?: CacheOptions) {
+    if (options) {
+      this.options = {
+        ...this.options,
+        ...options,
+      };
+    }
+
+    const weakThis = new WeakRef(this);
+    let timer = setInterval(() => {
+      const self = weakThis.deref();
+      if (self) {
+        self.purgeExpired();
+      } else {
+        clearInterval(timer);
+        timer = null;
+      }
+    }, this.options.purgeIntervalMs);
+
+    this.purgeExpiredTimer = timer;
+  }
+
+  async get(id: string, fetchFunc?: () => any, options?: CacheItemOptions) {
     if (!id) {
       return null;
     }
@@ -50,19 +83,46 @@ export class Neocache {
     return ret;
   }
 
-  set(id: string, data: any, options?: CacheOptions) {
+  set(id: string, data: any, options?: CacheItemOptions) {
     if (!id) {
       return;
     }
 
-    this.cache[id] = {
-      data,
-      expireAt: Date.now() + (options?.expireTimeMs ?? DEFAULT_EXPIRE_TIME_MS),
-    };
+    const expireTimeMs =
+      options?.expireTimeMs || this.options.defaultExpireTimeMs;
+    const expireAt = Date.now() + expireTimeMs;
+    this.cache[id] = { data, expireAt };
+
+    const timeKey = Math.floor(expireAt / this.options.purgeIntervalMs) + 1;
+    const keys = this.timeToKeyBucket.get(timeKey) ?? [];
+    keys.push(id);
+    this.timeToKeyBucket.set(timeKey, keys);
   }
 
   invalidate(id: string) {
     delete this.cache[id];
+  }
+
+  get length(): number {
+    return Object.keys(this.cache).length;
+  }
+
+  // Release expired items
+  purgeExpired() {
+    const now = Date.now();
+    const timeKey = Math.floor(now / this.options.purgeIntervalMs);
+    const keys = this.timeToKeyBucket.get(timeKey) ?? [];
+    for (const key of keys) {
+      this.invalidate(key);
+    }
+    this.timeToKeyBucket.delete(timeKey);
+  }
+
+  dispose() {
+    if (this.purgeExpiredTimer) {
+      clearInterval(this.purgeExpiredTimer);
+      this.purgeExpiredTimer = null;
+    }
   }
 }
 
